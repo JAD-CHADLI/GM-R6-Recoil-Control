68f5641b8e18ebfc85f4e795db68e3ae3f12421d
GAMMO <chadlijad00@gmail.com>
2025-12-15 20:48:49 +0100

    Update Program.cs

--- GM R6 Recoil Control/Program.cs
+++ GM R6 Recoil Control/Program.cs
@@ -9,7 +9,7 @@ namespace MouseSliderApp
         static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.aaa
+            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
